a77ec4ab568f9b348ef71eff1091dc019d6a8c79
Mochichi2003 <moyurusuto@gmail.com>
2020-02-24 11:43:49 +0900

    おみくじの木園部分ができた

--- scripts/hello.js
+++ scripts/hello.js
@@ -4,4 +4,24 @@ module.exports = (robot) => {
     const user_id = msg.message.user.id;
     msg.send(`Hello, <@${user_id}>`);
   });
+  robot.respond(/今日の天気/i, (msg) => {
+    const user_id = msg.message.user.id;
+    msg.send(`Hay, <@${user_id}>`);
+  })
+  robot.hear(/おみくじ/i, (msg) => {
+    const user_id = msg.message.user.id;
+    const camera_omikuzi = {
+      0: "α9m2",
+      1: "EOS R",
+      2: "α7r4",
+      3: "Sigma fp",
+      4: "EOS 1D m4"
+    }
+    var length = Object.keys(camera_omikuzi).length;//おみくじの数を取得
+
+    var random = Math.floor(Math.random() * length);//おみくじを選ぶ乱数を作成
+    console.log(random + "  カメラ = " + camera_omikuzi[random + 1])
+    msg.send(random + ` <@${user_id}>の今日のラッキーカメラは` + camera_omikuzi[random] + 'だよ');
+
+  })
 }
